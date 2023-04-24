aeb98283a03fd9fe9be8cde3c3e7146460d69aaf
cpalaciosvSOEN <cpalaciosv@soen.pe>
2023-04-24 10:21:01 -0500

    feat(1st step): Before daily meet

--- js/app.js
+++ js/app.js
@@ -5,23 +5,10 @@ let view = function (){
             this.rootElement = $('#root');
             this.getInit()
         },
-        getInit: async function(){
-            try{
-                this.rootElement.append(
-                    this.templateView({
-                        title: 'Rick and Morty',
-                        subtitle: 'The best TV show',
-                        description: 'Rick and Morty is an American adult animated science fiction sitcom created by Justin Roiland and Dan Harmon for Cartoon Network\'s nighttime Adult Swim programming block. The series follows the misadventures of cynical mad scientist Rick Sanchez and his good-hearted but fretful grandson Morty Smith, who split their time between domestic life and interdimensional adventures.',
-                        body: [
-                            this.templateMenu(),
-                            this.templateRender()
-                        ]
-                    }),
-                )
-            }catch(error){
-                // BI.handleErrorNuevo(error)
-                console.error(error)
-            }
+        getInit: async function(data){
+            this.rootElement.append(
+                this.templateView()
+            )
         },
         // #endregion
         // #region templates
@@ -33,76 +20,36 @@ let view = function (){
         },
 
         templateView: function(context = this){
-            context.containerView = $('<div>').attr({ 
-                id: 'container',
-                class: 'container' 
+            context.btnCharacters = $('<button>').text('Characters').on('click', () => {
+
             })
-            context.containerView.append(
-                $('<h1>').text(context.title),
-                $('<h2>').text(context.subtitle),
-                $('<p>').text(context.description),
-                context.body
+            context.btnEpisodes = $('<button>').text('Episodes').on('click', () => {
+
+            })
+            context.btnLocations = $('<button>').text('Locations').on('click', () => {
+                
+            })
+
+            context.renderArea = $('<div>').attr({ id: 'js-view-render' })
+            
+            return $('<div>').append(
+                $('<h1>').text('Rick and Morty API'),
+                $('<div>').append(
+                    context.btnCharacters,
+                    context.btnEpisodes,
+                    context.btnLocations
+                ),
+                context.renderArea,
             )
-            return context.containerView
         },
         templateMenu: function(context = this){
-            context.buttonCharacters = $('<button>').text('Characters')
-            context.buttonLocation = $('<button>').text('Location')
-            context.buttonEpisodes = $('<button>').text('Episodes')
-
-            context.buttonCharacters.on('click', () => {
-                context.renderCharacters(context)
-            })
-
-            context.buttonLocation.on('click', () => {
-                context.renderLocation(context)
-            })
-
-            context.buttonEpisodes.on('click', () => {
-                context.renderEpisodes(context)
-            })
-
-            context.containerMenu = $('<div>').addClass('container')
-
-            return context.containerMenu.append(
-                context.buttonCharacters, 
-                context.buttonLocation, 
-                context.buttonEpisodes
-            )
         },
         templateRender: function(context = this){
-            context.containerRender = $('<div>').attr({ 
-                id: 'container',
-                class: 'js-render-data' 
-            })
-            return context.containerRender
         },
 
         templateCards: function(context = this){
-            context.containerCards = $('<div>').attr({
-                style: `
-                    display: flex;
-                    flex-wrap: wrap;
-                    justify-content: space-around;
-                `,
-                class: 'js-cards'
-            })
-
-            return context.containerCards 
         },
         templateCard: function(context = this){
-            context.containerCard = $('<div>').attr({
-                style: `
-                    width: 300px;
-                    height: 300px;
-                    border: 1px solid black;
-                    margin: 10px;
-                    padding: 10px;
-                `,
-                class: 'js-card'
-            })
-
-            return context.containerCard
         },
         // #endregion
         // #region API's requests 
@@ -110,6 +57,7 @@ let view = function (){
             let request = await axios.get('https://rickandmortyapi.com/api/character')
             return request.data;
         },
+        // #endregion
     }
 }
 
