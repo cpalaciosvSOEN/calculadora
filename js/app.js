b177847a2e2145198ff97d4ef0b6b1d99813c164
cpalaciosvSOEN <cpalaciosv@soen.pe>
2023-04-24 12:25:15 -0500

    feat(Create Structures)

--- js/app.js
+++ js/app.js
@@ -11,23 +11,56 @@ let view = function (){
             )
         },
         // #endregion
+        // #region handlers
+        handleShowCharacters: async function(){
+            let requestCharacters = await this.getCharacters()
+            console.log(requestCharacters)
+            this.renderArea.empty()
+            this.renderArea.append(
+                $('<div>').attr({ class: 'c-cards'}).append(
+                    requestCharacters.results.map(card => {
+                        return this.templateCardCharacter(card)
+                    })
+                )
+            )
+        },
+
+        handleShowEpisodes: async function(){
+            let requestEpisodes = await this.getEpisodes()
+            console.log(requestEpisodes)
+            this.renderArea.empty()
+            this.renderArea.append(
+                $('<div>').attr({ class: 'c-cards'}).append(
+                    requestEpisodes.results.map(card => {
+                        return this.templateCardEpisode(card)
+                    })
+                )
+            )
+        },
+
+        handleShowLocations: async function(){
+            let requestLocations = await this.getLocations()
+            console.log(requestLocations)
+            this.renderArea.empty()
+            this.renderArea.append(
+                $('<div>').attr({ class: 'c-cards'}).append(
+                    requestLocations.results.map(card => {
+                        return this.templateCardLocation(card)
+                    })
+                )
+            )
+        },
+
         // #region templates
-        templateCharacters: function(context = this){
-        },
-        templateEpisodes: function(context = this){
-        },
-        templateLocation: function(context = this){
-        },
-
         templateView: function(context = this){
-            context.btnCharacters = $('<button>').text('Characters').on('click', () => {
-
+            context.btnCharacters = $('<button>').text('Characters').on('click', async () => {
+                await this.handleShowCharacters()
             })
             context.btnEpisodes = $('<button>').text('Episodes').on('click', () => {
-
+                this.handleShowEpisodes()
             })
             context.btnLocations = $('<button>').text('Locations').on('click', () => {
-                
+                this.handleShowLocations()
             })
 
             context.renderArea = $('<div>').attr({ id: 'js-view-render' })
@@ -46,15 +79,59 @@ let view = function (){
         },
         templateRender: function(context = this){
         },
-
         templateCards: function(context = this){
         },
-        templateCard: function(context = this){
+        templateCardCharacter: function(data){
+            return $('<div>').attr({ class: 'c-card'}).append(
+                $('<div>').attr({ class: 'c-card__header'}).append(
+                    $('<h2>').text(data.name)
+                ),
+                $('<div>').attr({ class: 'c-card__body'}).append(
+                    $('<img>').attr({ src: data.image })
+                ),
+                $('<div>').attr({ class: 'c-card__footer'}).append(
+                    $('<button>').text('Show More')
+                )
+            )
+        },
+        templateCardEpisode: function(data){
+            return $('<div>').attr({ class: 'c-card'}).append(
+                $('<div>').attr({ class: 'c-card__header'}).append(
+                    $('<h2>').text(`${data.name} - ${data.episode}`)
+                ),
+                $('<div>').attr({ class: 'c-card__body'}).append(
+                    $('<img>').attr({ src: data.image })
+                ),
+                $('<div>').attr({ class: 'c-card__footer'}).append(
+                    $('<button>').text('Show More')
+                )
+            )
+        },
+        templateCardLocation: function(data){
+            return $('<div>').attr({ class: 'c-card'}).append(
+                $('<div>').attr({ class: 'c-card__header'}).append(
+                    $('<h2>').text(data.name)
+                ),
+                $('<div>').attr({ class: 'c-card__body'}).append(
+                    $('<img>').attr({ src: data.image })
+                ),
+                $('<div>').attr({ class: 'c-card__footer'}).append(
+                    $('<button>').text('Show More')
+                )
+            )
         },
         // #endregion
         // #region API's requests 
-        getCharacters: async function(){
-            let request = await axios.get('https://rickandmortyapi.com/api/character')
+        getCharacters: async function(params = {}){
+            let request = await axios.get('https://rickandmortyapi.com/api/character', params)
+            return request.data;
+        },
+        getEpisodes: async function(params = {}){
+            let request = await axios.get('https://rickandmortyapi.com/api/episode', params)
+            return request.data;
+        },
+        getLocations: async function(params = {}){
+            let request = await axios.get('https://rickandmortyapi.com/api/location', params)
             return request.data;
         },
         // #endregion
